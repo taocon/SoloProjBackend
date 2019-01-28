1e8a056feb5da8fe7312f04c1a40a1cfe38c78e8
taocon <conrad_tao@hotmail.com>
2019-01-28 01:37:33 +0000

    workfromhome

--- target/Soloproject/WEB-INF/classes/import.sql
+++ target/Soloproject/WEB-INF/classes/import.sql
@@ -4,9 +4,27 @@ INSERT INTO ACCOUNT (accountId,name,password) VALUES (3, 'sdsdsdsd','jgordon')
 INSERT INTO ACCOUNT (accountId,name,password) VALUES (4, 'chadmayr','jgordon')
 
 
+																										
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 1, 'Abyssal Blade' , 25,0,0,0,10,0,0,250,10,0,0,0,0,0,'abyssal_blade_lg')
 
-INSERT INTO ITEM (itemId,itemName,atkdmg,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 1, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,'abyssal_blade_lg')
-
-INSERT INTO ITEM (itemId,itemName,atkdmg,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 2, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,'aegis_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 2, 'Aegis of the Immortal' ,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,'aegis_lg')
  
-INSERT INTO ITEM (itemId,itemName,atkdmg,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 3, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,'ancient_janggo_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 3, 'Arcane Boots' , 0,0,15%,0,0,0,0,0,0,250,0,0,0,0,'arcane_boots_lg')
+
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 3, 'Armlet of Mordiggian',9,25,0,5,0,0,0,0,5,0,0,0,0,0,'armlet_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'Assault Cuirass' , 0,55,0,15,0,0,0,0,0,0,0,0,0,0,'assault_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'Skull Basher' , 25,0,0,0,10,0,0,0,0,0,0,0,0,0,'basher_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'Belt of Strength' , 0,0,0,0,6,0,0,0,0,0,0,0,0,0,'belt_of_strength_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'Battlefury' , 45,0,0,0,0,0,0,0,7.5,0,3.75,0,0,0,'bfury_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'blac_king_bar_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'black_mail_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'blade_of_alacrity_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'blades_of_attack_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'blink_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'bloodstone_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'boots_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'boots_of_elves_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'bottle_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'bracer_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'bloodstone_lg')
+INSERT INTO ITEM (itemId,itemName,atkdmg,atkspd,ms,armr,str,dex,intell, hp, hpreg, mana, manareg, magres,statres, spellamp,itemImg) VALUES ( 4, 'DEFAULT' , 0,0,0,0,0,0,0,0,0,0,0,0,0,0,'bloodstone_lg')
